e4682c6bbb850cf5667406b1af808b4e142a448b
greeshiee <grepra17@gmail.com>
2025-04-19 17:26:45 -0500

    initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p className='text-5xl'>Hello</p>
+    </div>
+  )
+}
+
+export default App
